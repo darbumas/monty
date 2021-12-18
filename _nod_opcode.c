c06e4e0966f12b3903090621dd5db4bcb4c29d2a
darbumas <2314@holbertonschool.com>
2021-12-18 04:35:51 +0000

    useless func

---
+++ _nod_opcode.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * _nop_opcode - ƒ() iddle.
+ * @stackPtr: pointer to the top of the stack.
+ * @l_num: line number in the file.
+ * Return: nothing
+ */
+void _nop_opcode(__attribute__((unused)) stack_t **stackPtr, unsigned int l_num)
+{
+	l_num += 0;
+}
